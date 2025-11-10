d20833240c6d43e58a3dd8095e12853ff64daf30
xXViktor123Xx <viktor.soderquist@skola.taby.se>
2025-11-10 15:06:54 +0100

    Update sten_sax_påse.py
    random bibliotek + Spelarens namn och antal rundor inmatning

--- sten_sax_påse.py
+++ sten_sax_påse.py
@@ -1,1 +1,7 @@
-Lilllsssss
+#importerar random biblioteket
+import random
+
+#Här matar man in spelarens namn och antal rundor
+print("Hej! Jag är ett Sten Sax Påse program.")
+namn = input("Vad heter du? ")
+antal = int(input("Hur många rundor vill du spela? ")) 
