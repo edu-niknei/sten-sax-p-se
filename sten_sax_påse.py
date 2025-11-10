f12acad7221d9b4641f7a7a144e430e9748f72da
xXViktor123Xx <viktor.soderquist@skola.taby.se>
2025-11-10 15:15:14 +0100

    Update sten_sax_påse.py
    Lade till poängräkning samt alternativ för spelet

--- sten_sax_påse.py
+++ sten_sax_påse.py
@@ -5,3 +5,11 @@ import random
 print("Hej! Jag är ett Sten Sax Påse program.")
 namn = input("Vad heter du? ")
 antal = int(input("Hur många rundor vill du spela? ")) 
+
+#poängräkning
+poang_spelare = 0
+poang_puton = 0
+
+#alternativ för spelet
+val = ["sten", "sax", "påse"]
+
