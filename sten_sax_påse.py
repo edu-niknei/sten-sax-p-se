7c5d27783008b30b948149f516980bf28ccecd19
xXViktor123Xx <viktor.soderquist@skola.taby.se>
2025-11-13 12:48:01 +0100

    Huvudloopen för spelet
    Lade till inmatning för spelarens val samt Om man skriver fel så mer man en fel meddelande. Viktor/Nicklas

--- sten_sax_påse.py
+++ sten_sax_påse.py
@@ -5,11 +5,18 @@ import random
 print("Hej! Jag är ett Sten Sax Påse program.")
 namn = input("Vad heter du? ")
 antal = int(input("Hur många rundor vill du spela? ")) 
-
+ 
 #poängräkning
 poang_spelare = 0
 poang_puton = 0
 
 #alternativ för spelet
 val = ["sten", "sax", "påse"]
+
+#huvudloopen för spelet
+for runda in range(antal):
+    spelare = input("Välj sten / sax / påse: ").lower()
+    while spelare not in val:
+        print("Fel! skriv sten, sax eller påse")
+        spelare = input("Försök igen: ").lower()
 
