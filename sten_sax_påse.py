e973b8aaf0a8bfde8e1eb93996ca173dea768231
edu-niknei <nikolas.neimanis@skola.taby.se>
2025-11-17 15:06:55 +0100

    Update sten_sax_påse.py
    Lade till ett program som testar om antal är heltal + lade till avgörande av vinnare i huvudloopen för spelet.

--- sten_sax_påse.py
+++ sten_sax_påse.py
@@ -4,7 +4,17 @@ import random
 #Här matar man in spelarens namn och antal rundor
 print("Hej! Jag är ett Sten Sax Påse program.")
 namn = input("Vad heter du? ")
-antal = int(input("Hur många rundor vill du spela? ")) 
+
+#Antal rundor + test om val är ett heltal
+while True:
+    try:
+        antal = int(input("Hur många rundor vill du spela? "))
+        if antal > 0:
+            break
+        else:
+            print("Skriv ett heltal större än 0")
+    except ValueError:
+        print("Fel! Du måste skriva ett heltal, Försök igen.")
  
 #poängräkning
 poäng_spelare = 0
@@ -19,14 +29,23 @@ for runda in range(antal):
     while spelare not in val:
         print("Fel! skriv sten, sax eller påse")
         spelare = input("Försök igen: ").lower()
-dator = random.choice(val)
+    dator = random.choice(val)
 
-#Avgöra vinnare och förlorare
-if spelare == dator:
-    print("oavgjort!")
-elif (spelare == "sten" and dator == "sax" or spelare == "sax" and dator == "påse" or spelare == "påse" and dator == "sten"):
-    print(f"{namn} Vann rundan!")
-    poäng_spelare += 1
-else: 
-    print("Datorn vann rundan!")
-    poäng_dator += 1
+    #Avgöra vinnare och förlorare
+    if spelare == dator:
+        print("oavgjort!")
+    elif (spelare == "sten" and dator == "sax" or spelare == "sax" and dator == "påse" or spelare == "påse" and dator == "sten"):
+        print(f"{namn} Vann rundan!")
+        poäng_spelare += 1
+    else: 
+        print("Datorn vann rundan!")
+        poäng_dator += 1
+
+#Slutresultat
+print("Resultat av spelet")
+if poäng_spelare > poäng_dator:
+    print(f"{namn} vann spelet!")
+elif poäng_dator > poäng_spelare:
+    print("Datorn vann spelet!")
+else:
+    print("spelet är oavgjort.")
