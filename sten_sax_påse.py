96ce391c2f15f1fe9538ccf922fda2df05e52133
xXViktor123Xx <viktor.soderquist@skola.taby.se>
2025-12-01 15:21:00 +0100

    Fixat lite små saker, gjort så att påse inte är buggat längre
    Datorn är fixat

--- sten_sax_påse.py
+++ sten_sax_påse.py
@@ -24,16 +24,20 @@ spelomgång = 1
 #alternativ för spelet
 val = ["sten", "sax", "påse"]
 
+
 #huvudloopen för spelet
 for runda in range(antal):
     spelare = input(f"spelomgång {spelomgång}\n---------\n Välj sten / sax / påse: ").lower()
     while spelare not in val:
         print("Fel! skriv sten, sax eller påse")
+#Datorns val
+        spelare = input(f"Välj sten / sax / påse: ").lower()
+    dator = random.choice(val)
 
     #Avgöra vinnare och förlorare
     if spelare == dator:
         print("\noavgjort!\n")
-    elif (spelare == "sten" and dator == "sax" or spelare == "sax" and dator == "påse" or spelare == "påse" and dator == "sten"):
+    elif (spelare == "sten" and dator == "sax" or spelare == "sax" and dator == "påse" or spelare == "påse" and dator == "sten" ):
         print(f"\n{namn} är vinnaren av spelomgång {spelomgång}!\n")
         poäng_spelare += 1
     else: 
@@ -42,16 +46,17 @@ for runda in range(antal):
 
     #Räknar spelomgångar
     spelomgång += 1
+# Slutresultat – vinnaren skrivs först
 
-#Slutresultat
+if poäng_spelare > poäng_dator:
+    print(f"\n{namn} vann spelet!\n")
+elif poäng_dator > poäng_spelare:
+    print("\nDatorn vann spelet!\n")
+else:
+    print("\nSpelet är oavgjort.\n")
+
 print("Spelresultat\n=============")
 print(f"{namn}: {poäng_spelare}p")
 print(f"dator: {poäng_dator}p\n")
-if poäng_spelare > poäng_dator:
-    print(f"{namn} vann spelet!")
-elif poäng_dator > poäng_spelare:
-    print("Datorn vann spelet!")
-else:
-    print("spelet är oavgjort.")
 
 print("programmet avslutades normalt")
